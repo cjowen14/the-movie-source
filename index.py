c1b1931e584cd04b78b976bbc987350dd0691aad
Cory Owen <cjowen14@outlook.com>
2022-03-22 09:32:56 -0600

    Added login/logout and registration

--- index.py
+++ index.py
@@ -2,10 +2,55 @@ from flask import Flask, render_template, request, flash, session, url_for, redi
 import requests
 from flask_sqlalchemy import SQLAlchemy
 import secret
+import psycopg2
 app = Flask(__name__)
 db = SQLAlchemy()
 
+app.config['SQLALCHEMY_DATABASE_URI'] = secret.uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.app = app
+db.init_app(app)
+
+app.secret_key = secret.secret_key
 api_key = secret.api_key
+
+## CREATE USER CLASS
+class User(db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_first_name = db.Column(db.String(255))
+    user_last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f"{self.user_first_name} {self.user_last_name}"
+
+
+class Ratings(db.Model):
+    __tablename__ = "ratings"
+
+    rating_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    movie_id = db.Column(db.Integer)
+    rating_score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"User {self.user_id} rated {self.rating_score}"
+
+
+class Reviews(db.Model):
+    __tablename__ = "reviews"
+
+    review_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    movie_id = db.Column(db.Integer)
+    rating_id = db.Column(db.Integer, db.ForeignKey(Ratings.rating_id))
+    review = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"{self.review}"
 
 
 ## GET POPULAR MOVIES AND DISPLAY ON HOME PAGE
@@ -36,16 +81,6 @@ def search_results_B(movie_id):
 
 @app.route('/search-results/<user_search>', methods=["POST"])
 def search_results_C(user_search):
-    user_search = request.form['search']
-    return redirect(url_for('search_results2',user_search=user_search))
-
-@app.route('/register', methods=["POST"])
-def search_results_D():
-    user_search = request.form['search']
-    return redirect(url_for('search_results2',user_search=user_search))
-
-@app.route('/login', methods=["POST"])
-def search_results_E():
     user_search = request.form['search']
     return redirect(url_for('search_results2',user_search=user_search))
 
@@ -87,12 +122,72 @@ def movie_info(movie_id):
     
     return render_template('movie-info.html', results=results, actors=actors, director=director)
 
+
+
+
+
 @app.route('/login')
 def show_login():
     return render_template('login.html')
+
+@app.route('/login', methods=["POST"])
+def process_login():
+    email = request.form['email']
+    password = hash(request.form['password'])
+
+    users = User.query.all()
+    for user in users:
+        if user.email == email and hash(user.password) == password:
+            print("You are logged in!")
+            session['email'] = email
+            session['name'] = user.user_first_name
+            print(session['name'])
+        elif user.email == email and not hash(user.password) == password:
+            print("Incorrect Password")
+        else:
+            print("Email not found")
+    
+    return redirect(url_for('home'))
+
+
+@app.route('/logout')
+def process_logout():
+    session.pop('email', None)
+    session.pop('name', None)
+    return redirect(url_for('home'))
+
 
 @app.route('/register')
 def show_register():
     return render_template('register.html')
 
+@app.route('/register', methods=["POST"])
+def process_register():
+    first_name = request.form['first']
+    last_name = request.form['last']
+    email = request.form['email']
+    password = request.form['password']
 
+    new_user = User(user_first_name=first_name, user_last_name=last_name, email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+
+@app.route('/reviews/<movie_id>')
+def see_reviews(movie_id):
+    reviews = Reviews.query.all()
+    review_list = []
+    for review in reviews:
+        if str(review.movie_id) == str(movie_id):
+            review_list.append(review)
+    return render_template('reviews.html', reviews=review_list)
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
